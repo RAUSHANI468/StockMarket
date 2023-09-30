dcd3cdd4e5b75998585cab5104487857b7e90bb4
RAUSHANI468 <99281773+RAUSHANI468@users.noreply.github.com>
2023-10-01 00:54:36 +0530

    Update projectJava.java

--- projectJava.java
+++ projectJava.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.raushani;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-/**
- *
- * @author Guddan
- */
+
 public class projectJava {
     String email,username;
     String password;
